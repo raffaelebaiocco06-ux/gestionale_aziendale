466c3de3d6bff5a133b61d461d1fe94af44a8c94
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-05-13 12:36:24 +0200

    add mezzo funzionante

--- back-end/aziendale/src/main/java/gestionale/aziendale/controller/MezzoController.java
+++ back-end/aziendale/src/main/java/gestionale/aziendale/controller/MezzoController.java
@@ -1,10 +1,58 @@
 package gestionale.aziendale.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import gestionale.aziendale.entities.Mezzo;
+import gestionale.aziendale.payload.MezzoDTO;
+import gestionale.aziendale.service.MezzoService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/mezzi")
 public class MezzoController {
+    private final MezzoService mezzoService;
+
+    public MezzoController(MezzoService mezzoService) {
+        this.mezzoService = mezzoService;
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mezzo save(@RequestBody @Validated MezzoDTO body) {
+        return this.mezzoService.save(body);
+    }
+
+    @GetMapping
+    public Page<Mezzo> getAllMezzi(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy
+    ) {
+        return this.mezzoService.findAll(page, size, sortBy);
+    }
+
+    @GetMapping("/{id}")
+    public Mezzo findById(@PathVariable UUID id) {
+        return this.mezzoService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Mezzo findByIdAndUpdate(@PathVariable UUID id, @RequestBody @Validated MezzoDTO body) {
+        return this.mezzoService.findByIdAndUpdate(id, body);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findByIdAndDelete(@PathVariable UUID id) {
+        this.mezzoService.findByIdAndDelete(id);
+    }
+
+    @GetMapping("/targa/{targa}")
+    public Mezzo findByTarga(@PathVariable String targa) {
+        return this.mezzoService.findByTarga(targa);
+    }
 }
+
--- back-end/aziendale/src/main/java/gestionale/aziendale/service/MezzoService.java
+++ back-end/aziendale/src/main/java/gestionale/aziendale/service/MezzoService.java
@@ -2,9 +2,17 @@ package gestionale.aziendale.service;
 
 
 import gestionale.aziendale.entities.Mezzo;
+import gestionale.aziendale.exception.BadRequEx;
+import gestionale.aziendale.exception.NotFound;
 import gestionale.aziendale.payload.MezzoDTO;
 import gestionale.aziendale.repository.MezzoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class MezzoService {
@@ -14,6 +22,43 @@ public class MezzoService {
         this.mezzoRepository = mezzoRepository;
     }
     public Mezzo save(MezzoDTO body){
-        if(this.mezzoRepository.existsByTarga(body.targa())) throw new 
+        if(this.mezzoRepository.existsByTarga(body.targa())) throw new BadRequEx("la targa " + body.targa() + " è già in uso!");
+        Mezzo nuovoMezzo= new Mezzo(body.targa(), body.marca(), body.modello(),body.tipo(), body.anno(), body.assicurazioneScadenza(),body.bolloScadenza(),body.revisioneScadenza());
+        return nuovoMezzo;
+    }
+    public Page<Mezzo> findAll(int page, int size, String sortBy) {
+        if (size > 100 || size < 0) size = 10;
+        if (page < 0) page = 0;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return this.mezzoRepository.findAll(pageable);
+    }
+    public Mezzo findById(UUID Id) {
+        return this.mezzoRepository.findById(Id).orElseThrow(() -> new NotFound("Mezzo non trovato: "+ Id));
+    }
+    public Mezzo findByIdAndUpdate(UUID Id, MezzoDTO body){
+
+        Mezzo trovato= this.findById(Id);
+        if(!trovato.getTarga().equals(body.targa())){
+            if(this.mezzoRepository.existsByTarga(body.targa())) throw new BadRequEx("la Targa "+ body.targa()+"e' gia' in uso");
+        }
+        trovato.setTarga(body.targa());
+        trovato.setMarca(body.marca());
+        trovato.setModello(body.modello());
+        trovato.setTipo(body.tipo());
+        trovato.setAnno(body.anno());
+        trovato.setAssicurazioneScadenza(body.assicurazioneScadenza());
+        trovato.setBolloScadenza(body.bolloScadenza());
+        trovato.setRevisioneScadenza(body.revisioneScadenza());
+
+        Mezzo nuovo = this.mezzoRepository.save(trovato);
+        return nuovo;
+    }
+    public void findByIdAndDelete(UUID Id) {
+        Mezzo trovato = this.findById(Id);
+        this.mezzoRepository.delete(trovato);
+    }
+
+    public Mezzo findByTarga(String targa){
+        return this.mezzoRepository.findByTarga(targa).orElseThrow( ()-> new NotFound("la Targa "+ targa +"non c'e'"));
     }
 }
