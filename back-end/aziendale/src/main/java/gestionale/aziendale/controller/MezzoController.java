b152905bd6d69954f9a3d300e03b626bfd712f37
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-05-14 16:22:07 +0200

    add autorization mezzocontroller

--- back-end/aziendale/src/main/java/gestionale/aziendale/controller/MezzoController.java
+++ back-end/aziendale/src/main/java/gestionale/aziendale/controller/MezzoController.java
@@ -6,6 +6,7 @@ import gestionale.aziendale.payload.MezzoDTO;
 import gestionale.aziendale.service.MezzoService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,11 +22,13 @@ public class MezzoController {
     }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public Mezzo save(@RequestBody @Validated MezzoDTO body) {
         return this.mezzoService.save(body);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'OPERATORE')")
     public Page<Mezzo> getAllMezzi(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
@@ -35,22 +38,26 @@ public class MezzoController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'OPERATORE')")
     public Mezzo findById(@PathVariable UUID id) {
         return this.mezzoService.findById(id);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public Mezzo findByIdAndUpdate(@PathVariable UUID id, @RequestBody @Validated MezzoDTO body) {
         return this.mezzoService.findByIdAndUpdate(id, body);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void findByIdAndDelete(@PathVariable UUID id) {
         this.mezzoService.findByIdAndDelete(id);
     }
 
     @GetMapping("/targa/{targa}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'OPERATORE')")
     public Mezzo findByTarga(@PathVariable String targa) {
         return this.mezzoService.findByTarga(targa);
     }
